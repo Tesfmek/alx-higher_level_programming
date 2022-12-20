574d9dbbf101aef20e840947f0842e142a23239b
Tesfmek <110990834+Tesfmek@users.noreply.github.com>
2022-12-20 15:01:08 +0200

    Add files via upload

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *tmp;
+
+	if (!head)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+
+	if (*head)
+	{
+		tmp = *head;
+		if (number <= tmp->n)
+		{
+			new->next = tmp;
+			*head = new;
+		}
+		else
+		{
+			while (number > (tmp->next)->n)
+			{
+				tmp = tmp->next;
+				if (!tmp->next)
+					break;
+			}
+			new->next = tmp->next;
+			tmp->next = new;
+		}
+	}
+	else
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	return (new);
+}
